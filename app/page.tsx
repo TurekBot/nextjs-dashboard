de1da01acf51f08603db3bf8d675d072984a5e46
Bradley Turek <TurekBot@gmail.com>
2023-11-12 21:44:04 -0700

    feat: completed chapter 3

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import styles from '@/app/ui/home.module.css'
 import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
 
 import Link from 'next/link';
 
@@ -29,7 +30,20 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+          src="/hero-desktop.png"
+          width={1000}
+          height={760}
+          className='hidden md:block'
+          alt="Screenshots of the dashboard project showing desktop and mobile versions"
+          />
+          <Image 
+          src="/hero-mobile.png"
+          height={620}
+          width={560}
+          className='block md:hidden'
+          alt='Screenshots of the dashboard project showing how it looks on a mobile phone'
+          />
         </div>
       </div>
     </main>
